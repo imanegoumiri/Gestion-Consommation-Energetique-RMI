2095ae2a44c99c9818e5272b856ce11d7b30c0f8
Mariamkazaz <168235610+Mariamkazaz@users.noreply.github.com>
2025-05-11 23:20:36 +0200

    Delete Surveillance directory

--- Surveillance/SurveillanceService.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface SurveillanceService extends Remote {
-    void envoyerDonnees(String appareilId, double consommation) throws RemoteException;
-}
